ae9dedf0fa682f81d70e624bd82f7013ae4e68ce
ARKA1112 <bagha92@gmail.com>
2022-11-18 15:26:47 +0530

    .

---
+++ vector2d_v0.py
@@ -0,0 +1,41 @@
+from array import array
+import math
+
+class Vector2d:
+    typecode = 'd'
+    __match_args__ = ('x', 'y')
+    def __init__(self, x, y):
+        self.__x = float(x)
+        self.__y = float(y)
+
+    def __iter__(self):
+        return (i for i in (self.__x, self.__y))
+    
+    def __repr__(self) -> str:
+        class_name = type(self).__name__
+        return '{}({!r}, {!r})'.format(class_name, *self)
+    
+    def __str__(self):
+        return str(tuple(self))
+    
+    def __hash__(self) -> int:
+        return hash((self.__x, self.__y))
+    
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(array(self.typecode, self)))
+    
+    def __abs__(self):
+        return math.hypot(self.__x, self.__y)
+    
+    def __eq__(self, o) -> bool:
+        return tuple(self) == tuple(o)
+    
+    def __bool__(self):
+        return bool(abs(self))
+    @property     #add this method as property otherwise it will be a bound method
+    def x(self):
+        return self.__x
+    
+    
+    def __complex__(self):
+        return complex(self.__x,self.__y)
