5e4e18f6b9afd2cececef5555b64e15e90e4a9ab
ARKA1112 <bagha92@gmail.com>
2022-12-04 13:40:10 +0530

    .

--- vector_v1.py
+++ vector_v1.py
@@ -1,16 +1,20 @@
 from array import array
+import itertools
 import math
-
+from collections import abc
 class Vector:
     __match_args__ = ('x','y','z','t')
-    def __init__(self,*args) -> None:
-            self.args = args
+    def __init__(self,args) -> None:
+            self.args = [i for i in args]
 
     def __repr__(self) -> str:
             return f'{self.__class__.__name__}{self.args}'
 
     def __len__(self) -> int:
         return len(self.args)
+
+    def __iter__(self):
+        return iter(self.args)
 
     def __getitem__(self, index):
         for i in range(len(self.args)):
@@ -46,3 +50,45 @@ class Vector:
             outer_fmt = '({})'  
         components = (format(c, fmt_spec) for c in coords)  
         return outer_fmt.format(', '.join(components))
+    
+    def __add__(self, other):
+        try:
+            pairs = itertools.zip_longest(self.args,other,fillvalue=0.0)
+            return Vector(a+b for a,b in pairs)
+        except TypeError:
+            return NotImplemented
+        
+
+    def __radd(self, other):
+        return self.args + other
+
+    def __mul__(self, other):
+        return Vector(n*other for n in self.args)
+
+    def __rmul(self, other):
+        return [i*other for i in self.args]
+
+
+    def __matmul__(self, other):
+        if (isinstance(other, abc.Sized) and
+            isinstance(other, abc.Iterable)):
+            if len(self) == len(other):
+                return sum(a*b for a, b in zip(self,other))
+            else:
+                raise ValueError('@ requires vectors of equal length')
+        else:
+            return NotImplemented
+
+    def __rmatmul__(self, other):
+        return self @ other
+
+
+    #def __eq__(self, __o: object) -> bool:
+     #   return (len(self) == len(__o) and
+      #          all(a==b for a,b in zip(self,__o)))
+
+      #improved __eq__
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(self, Vector):
+            return (len(self) == len(__o) and all(a==b for a, b in zip(self, __o)))
