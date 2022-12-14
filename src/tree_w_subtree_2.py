6cef02501b9d04e4989679adee8018c51a6ab334
ARKA1112 <bagha92@gmail.com>
2022-12-14 19:30:06 +0530

    .

---
+++ src/tree_w_subtree_2.py
@@ -0,0 +1,21 @@
+"""subtree traverses only till the second level
+if we go even deepre we will nedd to add further loops"""
+def tree(cls):
+    yield cls.__name__, 0
+    yield from sub_tree(cls)
+
+def sub_tree(cls):
+    for sub_cls in cls.__subclasses__():
+        yield sub_cls.__name__, 1
+        for sub_sub_cls in sub_cls.__subclasses__():
+            yield sub_sub_cls.__name__, 2  #for second level
+
+
+def display(cls):
+    for cls_name, level in tree(cls):
+        indent = ' ' * 4 * level
+        print(f'{indent}{cls_name}')
+
+
+if __name__ == '__main__':
+    display(BaseException)
